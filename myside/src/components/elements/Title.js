f8d49fee0e141e3a66acdffba96bb43bd9dcfe20
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-11-22 15:34:42 +0900

    이미지 가져옴

--- myside/src/components/elements/Title.js
+++ myside/src/components/elements/Title.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Title = () => {
+/**
+ * 
+ * @설명 Jua 폰트를 사용하는 컴포넌트 입니다.
+ * @ex 내편찾기, 할인행사, 베스트 상품 ...
+ * @사용법 props로 안에 들어갈 텍스트를 넣어주시면 됩니다.
+ */
+const Title = ({ name }) => {
     return (
         <div>
-
+            <img src="/images/point.png" className="point" alt="point" />
+            <span className="Jua_title">
+                {name}
+            </span>
         </div>
     );
 };
